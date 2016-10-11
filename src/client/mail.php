4b0021e605304cf1cee759e938ee9654235bb785
oShine <oyjqdlp@126.com>
2016-10-11 11:18:36 +0800

    v2.0.1

--- src/client/mail.php
+++ src/client/mail.php
@@ -4,3 +4,11 @@
  * @author ouyangjunqiu
  * @version Created by 2016/10/11 09:44
  */
+
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+if (!$client->connect('127.0.0.1', 9501, -1))
+{
+    exit("connect failed. Error: {$client->errCode}\n");
+}
+$client->send(json_encode(array("exec"=>"test")));
+$client->close();
