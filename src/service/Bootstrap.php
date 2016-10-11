5077d1571d19ef49265afa5a7f59881a9f0e35f3
oShine <oyjqdlp@126.com>
2016-10-11 11:39:29 +0800

    fixed

--- src/service/Bootstrap.php
+++ src/service/Bootstrap.php
@@ -12,34 +12,47 @@ class Bootstrap {
 
     private static $serv = null;
 
-    public static function boot(){
-
+    public function __construct()
+    {
         if(self::$serv == null){
             self::$serv = new swoole_server("127.0.0.1", 9501);
-            $dispatch = new Dispatch();
-
             self::$serv->set(array(
                 'worker_num' => 8,   //工作进程数量
                 'daemonize' => true, //是否作为守护进程
             ));
-
-            $dispatch->register("test",function($data){
-                Logger::log("test");
-            });
-
-
-            self::$serv->on('receive', function ($serv, $fd, $from_id, $data) use($dispatch) {
-
-                Logger::log("1111");
-                $data = json_decode($data,true);
-                $command = $data["exec"];
-                if(!empty($command)){
-                    $dispatch->trigger($command,$data);
-                }
-
-            });
         }
+
         return self::$serv;
+
+    }
+
+    public static function boot(){
+
+        $dispatch = new Dispatch();
+
+        $dispatch->register("test",function($data){
+            Logger::log("test");
+        });
+
+
+        $dispatch->register("mail",function($data){
+
+        });
+
+
+
+        self::$serv->on('receive', function ($serv, $fd, $from_id, $data) use($dispatch) {
+
+            $data = json_decode($data,true);
+            $command = $data["exec"];
+            if(!empty($command)){
+                $dispatch->trigger($command,$data);
+            }
+
+        });
+
+        self::$serv->start();
+
     }
 
 
